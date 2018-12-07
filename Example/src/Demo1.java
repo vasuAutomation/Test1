d747c09718f105d9a30582452772e9296d094649
vkatragunta <vkatragunta@MH10003181000.corp.miraclesoft.com>
2018-12-07 16:27:29 +0530

    Commit Success

---
+++ Example/src/Demo1.java
@@ -0,0 +1,12 @@
+
+class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("hello world");
+      int a=5,b=5,c;
+      c=a+b;
+      System.out.println(c);
+	}
+
+}
